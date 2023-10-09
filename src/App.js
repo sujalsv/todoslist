48f4d78990c40a25490d6824f1148d8e48709f26
sujalsv <111581568+sujalsv@users.noreply.github.com>
2023-10-09 14:38:46 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,24 +1,94 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from "./logo.svg";
+import "./App.css";
+import Header from "./MyComponents/Header";
+import { Todos } from "./MyComponents/Todos";
+import { Footer } from "./MyComponents/Footer";
+import { About } from "./MyComponents/About";
+import { AddTodo } from "./MyComponents/AddTodo";
+import { useState, useEffect } from "react";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link
+} from "react-router-dom";
 
 function App() {
+  let initTodo;
+  if (localStorage.getItem("todos") === null) {
+    initTodo = [];
+  } else {
+    initTodo = JSON.parse(localStorage.getItem("todos"));
+  }
+
+  const onDelete = (todo) => {
+    console.log("I am ondelete of todo", todo);
+    setTodos(
+      todos.filter((e) => {
+        return e !== todo;
+      })
+    );
+    localStorage.setItem("todos", JSON.stringify(todos));
+  };
+
+  const addTodo = (title, desc) => {
+    console.log("i am adding this todo", title, desc);
+    let sno;
+
+    if (todos.length === 0) {
+      sno = 0;
+    } else {
+      sno = todos[todos.length - 1].sno + 1;
+    }
+
+    const myTodo = {
+      sno: sno,
+      title: title,
+      desc: desc,
+    };
+
+    setTodos([...todos, myTodo]);
+    console.log(myTodo);
+  };
+  const [todos, setTodos] = useState(initTodo);
+  useEffect(() => {
+    localStorage.setItem("todos", JSON.stringify(todos));
+  }, [todos]);
+
+  const MultipleComponents = () => (
+    <>
+    <AddTodo addTodo={addTodo} />
+    <Todos todos={todos} onDelete={onDelete} />
+
+    </>
+  );
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <Router>
+      <Header title="MyToDosList" searchBar={false} />
+      <Routes>
+          {/* <Route exact path="/" render={()=>  {
+            return(
+            <>
+            <AddTodo addTodo={addTodo}  />
+            <Todos todos={todos} onDelete={onDelete} />
+            </>)
+
+          }} /> */}
+          <Route exact path="/" Component={MultipleComponents} />
+          {/* <Route exact path="/" element={<Todos todos={todos} onDelete={onDelete} />} /> */}
+
+          <Route exact path="/about"  element={<About/>} />
+            {/* <About /> */}
+
+        </Routes>
+
+
+
+      <Footer />
+    </Router>
+    </>
   );
 }
 
