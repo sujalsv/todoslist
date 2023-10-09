7abbc5fddf2db2685ba2dee0d4591a1bd4af0285
sujalsv <111581568+sujalsv@users.noreply.github.com>
2023-10-09 14:33:02 +0530

    first commit

---
+++ src/MyComponents/About.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+export const About = () => {
+  return (
+    <div>
+      this is an about component
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Facere
+        voluptate incidunt minima fuga doloribus, amet provident doloremque
+        pariatur officia, ducimus, totam a harum velit sed nisi quasi? Molestias
+        quidem ab inventore. Praesentium fuga dolorum excepturi.
+      </p>
+    </div>
+  );
+};
